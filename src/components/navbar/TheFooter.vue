bb70172852e0ba82ab9cfeaf9b0934bb3c692f86
Animashaun Taofiq <animashauntaofiq@yahoo.com>
2023-02-20 23:45:32 +0100

    update: added icons to footer

--- src/components/navbar/TheFooter.vue
+++ src/components/navbar/TheFooter.vue
@@ -1,13 +1,16 @@
 <template>
-  <div>
-
+  <div class="section">
+    <div class="container">
+      <div class="flex items-center gap-2">
+        <span class="mdi mdi-email"></span>
+        <span class="mdi mdi-twitter"></span>
+        <span class="mdi mdi-linkedin"></span>
+        <span class="mdi mdi-instagram"></span>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
