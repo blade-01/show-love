33bd76eb524310e1452024c4c027aa3f9a8058de
Animashaun Taofiq <animashauntaofiq@yahoo.com>
2023-02-28 20:55:35 +0100

    update: added animation to needs section

--- src/components/home/HomeNeeds.vue
+++ src/components/home/HomeNeeds.vue
@@ -4,18 +4,27 @@
       class="text-center md:text-left md:grid md:gap-4 md:grid-cols-2 md:items-center md:place-items-center md:container md:bg-[#F3F2F5] md:rounded-[18px] 1xl:w-[1224px] md:py-[48px] md:px-[80px]"
     >
       <div class="container lg:w-[499px]">
-        <h3 class="title md:font-semibold lg:leading-[58.8px] lg:text-[42px]">
+        <h3
+          class="title md:font-semibold lg:leading-[58.8px] lg:text-[42px]"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom"
+        >
           What the world needs is Love and You
         </h3>
         <p
           class="font-normal text-[12px] lg:text-[18px] lg:leading-[28.8px] leading-[19.2px] py-[20px]"
+          data-aos="fade"
+          data-aos-delay="500"
         >
-          Plan ahead with us to create joyful moments in the homes of friends
-          and families, colleagues and work spaces in any event you want to
-          celebrate
+          Plan ahead with us to create joyful moments in the homes of friends and
+          families, colleagues and work spaces in any event you want to celebrate
         </p>
         <button
           class="btn mx-auto md:m-0 md:py-[22px] md:px-[48px] leading-[19.2px] md:font-semibold md:text-base"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom"
+          data-aos-delay="500"
+          data-aos-offset="0"
         >
           Get started
         </button>
