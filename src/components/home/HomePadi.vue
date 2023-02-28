7db2094a65acfde3b7399b2410835dfda5287616
Animashaun Taofiq <animashauntaofiq@yahoo.com>
2023-02-28 20:55:41 +0100

    update: added animation to padi section

--- src/components/home/HomePadi.vue
+++ src/components/home/HomePadi.vue
@@ -6,18 +6,26 @@
       <div class="px-[10%]">
         <h5
           class="title text-[28px] lg:text-[42px] lg:leading-[58.8px] md:font-semibold"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom"
         >
           Show some love to your padi of life
         </h5>
         <p
           class="font-normal text-[12px] lg:text-[18px] lg:leading-[28.8px] leading-[19.2px] py-[20px]"
+          data-aos="fade"
+          data-aos-delay="500"
         >
-          Gifts that suits every pocket; Show some love without breaking the
-          bank. With a list of premium curated gifts from the best brands within
-          a budget, your love can be expressed and felt for as low as 5k
+          Gifts that suits every pocket; Show some love without breaking the bank. With a
+          list of premium curated gifts from the best brands within a budget, your love
+          can be expressed and felt for as low as 5k
         </p>
         <button
           class="btn mx-auto md:ml-0 md:py-[22px] md:px-[48px] md:text-base"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom"
+          data-aos-delay="500"
+          data-aos-offset="0"
         >
           Get started
         </button>
