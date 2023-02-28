d66171ac230b551ba4fc03906e8bfd4d41f96d27
Animashaun Taofiq <animashauntaofiq@yahoo.com>
2023-02-28 20:55:53 +0100

    update: added animation to timeline section

--- src/components/home/HomeTimeline.vue
+++ src/components/home/HomeTimeline.vue
@@ -8,17 +8,27 @@
       >
         <h4
           class="font-bold text-[28px] leading-[33.6px] lg:text-[42px] lg:leading-[58.8px] md:font-semibold"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom"
         >
           In Every Timeline and Multiverse, your love counts
         </h4>
         <p
           class="text-[12px] text-[#2F2F2F] leading-[19.2px] font-normal my-[20px] lg:text-[18px] lg:leading-[28.8px]"
+          data-aos="fade"
+          data-aos-delay="500"
         >
-          We take the lead and make loving easy, from gift sourcing and sorting
-          to delivery. You can trust us to pick the perfect gift and deliver to
-          your beneficiary without having to provide their addresses.
+          We take the lead and make loving easy, from gift sourcing and sorting to
+          delivery. You can trust us to pick the perfect gift and deliver to your
+          beneficiary without having to provide their addresses.
         </p>
-        <button class="btn mx-auto md:ml-0 md:text-base">
+        <button
+          class="btn mx-auto md:ml-0 md:text-base"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom"
+          data-aos-delay="500"
+          data-aos-offset="0"
+        >
           <img src="/img/apple-google.svg" />
           Download the app
         </button>
