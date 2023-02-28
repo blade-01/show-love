2325bbd70e1726ffec57c8ccb8e0765d9f280c83
Animashaun Taofiq <animashauntaofiq@yahoo.com>
2023-02-28 19:59:34 +0100

    refactor: refactored main section

--- src/components/home/HomeMain.vue
+++ src/components/home/HomeMain.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="section lg:py-24 bg-[#644AE2]">
+  <div class="section main-bg lg:py-24 lg:pb-[124px] bg-[#644AE2]">
     <div class="container">
-      <div class="text-center px-2 lg:w-[863px] lg:mx-auto">
-        <h1
-          class="title text-[#FFFFFF] lg:text-[42px] lg:leading-[50.4px] lg:font-semibold"
+      <div class="text-center px-2 lg:w-[749px] lg:mx-auto">
+        <h2
+          class="title text-[#FFFFFF] lg:text-[42px] lg:leading-[50.4px] lg:font-semibold mb-6"
         >
           With just about any gift, make your thoughts count.
-        </h1>
+        </h2>
         <p
           class="font-normal text-[12px] leading-[19.2px] pt-[20px] text-[#FAFAFA] lg:text-[18px] lg:leading-[28px]"
         >
@@ -21,4 +21,8 @@
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.main-bg {
+  @apply bg-[url('/img/slant-bg.svg')] bg-no-repeat bg-bottom;
+}
+</style>
