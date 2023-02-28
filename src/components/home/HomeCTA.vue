473c563c72d8eb4905c65651bd2e3c40652c4365
Animashaun Taofiq <animashauntaofiq@yahoo.com>
2023-02-28 20:55:17 +0100

    update: added animation to CTA section

--- src/components/home/HomeCTA.vue
+++ src/components/home/HomeCTA.vue
@@ -7,20 +7,38 @@
       >
         <h1
           class="text-[#070707] mb-4 lg:mb-6 text-[42px] leading-[66.11px] lg:text-[72px] lg:leading-[113.33px] font-bold mt-12 lg:mt-0"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom"
         >
           Show <span class="text-primary">Some</span> Love
         </h1>
-        <p class="mb-4 lg:mb-6 text-priTex text-sm lg:text-lg">
-          We let you stay on top of your game by reminding you of Special dates
-          and anniversaries, so you never have to forget to plan ahead and show
-          your love through gifts.
+        <p
+          class="mb-4 lg:mb-6 text-priTex text-sm lg:text-lg"
+          data-aos="fade"
+          data-aos-delay="500"
+        >
+          We let you stay on top of your game by reminding you of Special dates and
+          anniversaries, so you never have to forget to plan ahead and show your love
+          through gifts.
         </p>
-        <button class="btn hover:scale-100 custom-shadow lg:font-bold">
+        <button
+          class="btn hover:scale-100 custom-shadow lg:font-bold"
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom"
+          data-aos-delay="500"
+          data-aos-offset="0"
+        >
           <img src="/img/small-barcode.svg" alt="small-barcode" />
           <span>GET THE APP</span>
         </button>
       </div>
-      <img src="/img/illus-01.svg" alt="illustration" />
+      <img
+        src="/img/illus-01.svg"
+        alt="illustration"
+        data-aos="fade-up"
+        data-aos-delay="650"
+        data-aos-duration="700"
+      />
     </div>
   </div>
 </template>
@@ -30,7 +48,7 @@
 <style scoped>
 .main-bg {
   background: #f3f2f5;
-  background-image: url("/img/main-bg.svg");
+  background-image: url('/img/main-bg.svg');
 }
 .custom-shadow {
   box-shadow: 0px 1px 24px rgba(162, 91, 255, 0.4);
