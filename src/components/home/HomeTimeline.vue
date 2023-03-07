696a1bf7826a466d4cf3e9dc342df96b8d8da43c
Animashaun Taofiq <animashauntaofiq@yahoo.com>
2023-03-07 16:22:42 +0100

    refactor: updated timeline section UI

--- src/components/home/HomeTimeline.vue
+++ src/components/home/HomeTimeline.vue
@@ -1,46 +1,36 @@
 <template>
-  <!-- .main-bg -->
-  <div class="pt-16 md:pb-16 bg-[#F3F2F5] scroll-pt-16" id="download">
-    <!-- md:bg-white md:rounded-[18.0923px] -->
-    <!-- 1xl:w-[1224px] -->
-    <div
-      class="md:text-left md:grid md:gap-14 md:grid-cols-2 md:items-center md:place-items-center md:container lg:pl-[80px]"
-    >
+  <div class="section bg-[#F3F2F5] scroll-pt-16" id="download">
+    <div class="container">
       <div
-        class="text-center md:text-left text-[#2F2F2F] px-[10%] md:px-[0] md:py-[5%] lg:py-[0] 1xl:w-[517px]"
+        class="md:text-left md:grid md:gap-14 md:grid-cols-2 md:items-center md:place-items-center md:container lg:pl-[80px]"
       >
-        <h4
-          class="font-bold text-[28px] leading-[33.6px] lg:text-[42px] lg:leading-[58.8px] md:font-semibold"
+        <div class="text-center md:text-left text-[#2F2F2F] 1xl:w-[517px]">
+          <h4
+            class="font-bold text-[28px] leading-[33.6px] lg:text-[42px] lg:leading-[58.8px] md:font-semibold"
+            data-aos="fade-up"
+            data-aos-anchor-placement="top-bottom"
+          >
+            In Every Timeline and Multiverse, your love counts
+          </h4>
+          <p
+            class="text-[12px] text-[#2F2F2F] leading-[19.2px] font-normal pt-[20px] lg:text-[18px] lg:leading-[29px]"
+            data-aos="fade"
+            data-aos-delay="500"
+          >
+            We take the lead and make loving easy, from gift sourcing and sorting to
+            delivery. <br />
+            You can trust us to pick the perfect gift and deliver to your beneficiary
+            without having to provide their addresses.
+          </p>
+        </div>
+        <img
+          class="w-full mt-10 md:mt-0 md:rounded-[1rem]"
+          src="/img/illus-03.svg"
           data-aos="fade-up"
-          data-aos-anchor-placement="top-bottom"
-        >
-          In Every Timeline and Multiverse, your love counts
-        </h4>
-        <p
-          class="text-[12px] text-[#2F2F2F] leading-[19.2px] font-normal my-[20px] lg:text-[18px] lg:leading-[28.8px]"
-          data-aos="fade"
-          data-aos-delay="500"
-        >
-          We take the lead and make loving easy, from gift sourcing and sorting
-          to delivery. You can trust us to pick the perfect gift and deliver to
-          your beneficiary without having to provide their addresses.
-        </p>
-        <button
-          class="btn mx-auto md:ml-0 md:text-base"
-          data-aos="fade-up"
-          data-aos-anchor-placement="top-bottom"
-          data-aos-delay="500"
-          data-aos-offset="0"
-        >
-          <img src="/img/apple-google.svg" />
-          Download the app
-        </button>
+          data-aos-delay="650"
+          data-aos-duration="600"
+        />
       </div>
-      <img
-        class="mt-20 md:mt-0 w-full md:rounded-[1rem]"
-        src="/img/illus-03.svg"
-      />
-      <!-- md:w-[520px] -->
     </div>
   </div>
 </template>
