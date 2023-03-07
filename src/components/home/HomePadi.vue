1ab62279601c74ea4663ded371f062001d29056c
Animashaun Taofiq <animashauntaofiq@yahoo.com>
2023-03-07 16:22:26 +0100

    refactor: updated padi section UI

--- src/components/home/HomePadi.vue
+++ src/components/home/HomePadi.vue
@@ -1,41 +1,37 @@
 <template>
   <div class="section bg-white">
-    <div
-      class="text-center md:text-left md:container md:grid md:gap-4 md:grid-cols-2 md:items-center md:place-items-center"
-    >
-      <div class="px-[10%] xl:w-[600px]">
-        <h5
-          class="title text-[28px] lg:text-[42px] lg:leading-[58.8px] md:font-semibold"
+    <div class="container">
+      <div
+        class="text-center md:text-left md:grid gap-4 grid-cols-2 items-center place-items-center"
+      >
+        <div class="max-w-[510px] mx-auto">
+          <h5
+            class="title text-[28px] lg:text-[42px] lg:leading-[58.8px] md:font-semibold"
+            data-aos="fade-up"
+            data-aos-anchor-placement="top-bottom"
+          >
+            Show some love to your padi of life
+          </h5>
+          <p
+            class="text-[12px] text-[#2F2F2F] leading-[19.2px] font-normal pt-[20px] lg:text-[18px] lg:leading-[29px]"
+            data-aos="fade"
+            data-aos-delay="500"
+          >
+            Gifts that suits every pocket; Show some love without breaking the bank.
+            <br />
+            With a list of premium curated gifts from the best brands within a budget,
+            your love can be expressed and felt for as low as 5k
+          </p>
+        </div>
+        <img
+          class="w-full mt-10 md:mt-0 order-first"
+          src="/img/illus-04.svg"
+          alt="Home Padi"
           data-aos="fade-up"
-          data-aos-anchor-placement="top-bottom"
-        >
-          Show some love to your padi of life
-        </h5>
-        <p
-          class="font-normal text-[12px] lg:text-[18px] lg:leading-[28.8px] leading-[19.2px] py-[20px]"
-          data-aos="fade"
-          data-aos-delay="500"
-        >
-          Gifts that suits every pocket; Show some love without breaking the
-          bank. With a list of premium curated gifts from the best brands within
-          a budget, your love can be expressed and felt for as low as 5k
-        </p>
-        <button
-          class="btn mx-auto md:ml-0 md:py-[22px] md:px-[48px] md:text-base"
-          data-aos="fade-up"
-          data-aos-anchor-placement="top-bottom"
-          data-aos-delay="500"
-          data-aos-offset="0"
-        >
-          Get started
-        </button>
+          data-aos-delay="650"
+          data-aos-duration="600"
+        />
       </div>
-      <img
-        class="w-full mt-20 md:mt-0 order-first"
-        src="/img/illus-04.svg"
-        alt="Home Padi"
-      />
-      <!-- lg:w-[600px] -->
     </div>
   </div>
 </template>
